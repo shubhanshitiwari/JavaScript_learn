3d2cce2b6989c5eae16e2e98ee9a153ec50ec294
Shubhanshi Tiwari <155320556+shubhanshitiwari@users.noreply.github.com>
2024-05-18 03:57:08 +0000

    datatype

---
+++ 01_basics/05_datatype.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all JS as a newer version 
+
+//alert("hello") 
+// inside a doc engine is inside the browser
+
+console.log(3+3);  console.log("shubhanshi")//poor read-ability
+
+//ecma standards 
+
+//datatypes
+let name="shubhanshi"
+let age=18
+let isLoggedIn=true
+let state;
+
+
+//number range =>2 raise to power 52-53
+//bigint => big company like meta netflix
+//string =>"something" 'some_thing'
+//boolean =>true/false
+//null => standalone value,representation of empty value
+//undefined => it is value no value is assigned
+//symbol =>component whille using react,figma to define individual element
+//symbols =>unique
+
+//object
+
+//function typeof =>datatype
+
+console.log(typeof age);
+console.log(typeof[age,state]);
+console.log(typeof state);
+console.log(typeof isLoggedIn);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+//null type =>object
