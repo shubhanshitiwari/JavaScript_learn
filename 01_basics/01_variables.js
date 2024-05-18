05f974d241cf6894d899052558e81ec47c7cb6e5
Shubhanshi Tiwari <155320556+shubhanshitiwari@users.noreply.github.com>
2024-05-18 03:25:54 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId=144553 //prefered to lock the value
+let accountEmail="happygmail.com" //memory allocation
+var accountPassword="12345"
+accountCity ="Pune" //
+
+/*var  vs let
+scope--{}
+prefer not to use var cause' block and functional scope*/
+
+
+/*accountId=3 not allowed
+console.log(accountId); Assignment to constant variable.*/
+
+console.log(accountId);
+accountEmail="sadgmail.com"
+accountPassword=232323
+accountCity="Bhopal"
+let accountState; //undefined: value is nnot defined
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
