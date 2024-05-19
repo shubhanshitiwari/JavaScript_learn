6f20aaea3d0ad84ba163afb52b37ea19f9630691
Shubhanshi Tiwari <155320556+shubhanshitiwari@users.noreply.github.com>
2024-05-19 12:30:16 +0000

    The increment (++) operator increments (adds one to) its operand and returns the value before or after the increment, depending on where the operator is placed.

---
+++ 01_basics/07_operations.js
@@ -0,0 +1,66 @@
+// operation 
+let value= 3
+console.log(value);
+let negvalue=-value
+console.log(negvalue);
+
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2*2); //4
+// console.log(2**2); //4
+// console.log(2/3); // 0.666....
+// console.log(2%3); //2
+// console.log(2//3); //there is no such double divisor in js
+
+// let str1="shubhanshi"
+// let str2=" tiwari"
+// console.log(str1+str2);
+
+// const str1="shubhanshi"
+// const str2=" tiwari"
+// str3=str1+str2
+// console.log(str3);
+
+// console.log(2+1); //3
+// console.log("2"+1); //21
+// console.log(2+"1"); //21
+// console.log(2+1+"3"); //33
+// console.log("2"+1+3); //213
+// console.log(4+"2"+1); //421
+// console.log(2+1+5+6); //14
+// console.log(2+1+"5"+"6"); //356
+// {left to right} emca script --browser implementation
+
+console.log(3+4*5%3); //5
+
+// console.log(true); //true
+// console.log(+true); //1
+// console.log(true+); //errorExpression expected
+
+let num1,num2,num3
+num1=num2=num3=8+8
+console.log(num1);
+
+//increment 
+// let gameCounter =100;
+// console.log(gameCounter); //100
+// let y=gameCounter++; // post increment 
+// console.log(y);  //100
+// console.log(gameCounter); //101
+
+let gameCounter =100;
+console.log(gameCounter); //100
+let y=++gameCounter; // post increment //
+console.log(y); //101
+console.log(gameCounter); //101
+
+
+// per increase then use 
+// post use then increase
+
+
+
+
+
+
+
