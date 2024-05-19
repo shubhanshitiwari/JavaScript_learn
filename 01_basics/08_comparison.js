f04fad940a1fcd766cbc963be241474df26fcaa4
Shubhanshi Tiwari <155320556+shubhanshitiwari@users.noreply.github.com>
2024-05-19 12:53:19 +0000

    comparsion of datatype

---
+++ 01_basics/08_comparison.js
@@ -0,0 +1,30 @@
+// comparsion of datatype
+
+// console.log(2>1); //true
+// console.log(2<1); //false
+// console.log(2>=1); //true
+// console.log(2<=1); //false
+// console.log(2==1); // false
+// console.log("2">"1"); //true
+// console.log("2">1); // true
+// console.log(2==2); //true  
+
+// console.log(null>0); //false
+// console.log(null==0); //false
+// console.log(null>=0); // true
+// console.log(null<=0); //true 
+
+// the reason is that equality check == and camparsion >< >= <= work differently 
+// null ==0 is false but null>=0 is true
+
+// console.log(undefined>0); //false
+// console.log(undefined==0); //false
+// console.log(undefined>=0); // false
+// console.log(undefined<=0); // false
+
+// === not only value but datatype too 
+
+console.log(undefined===0); //false 
+
+
+
